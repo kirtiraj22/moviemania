90a974ab40007ff9c131d19ad1c6a743bfdedd13
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-07-20 23:29:15 +0530

    initialized header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,71 @@
+import { useEffect, useState } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
+
 import "./style.scss";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
+
 const Header = () => {
-  return <div>Header</div>;
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
+
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+
+  return (
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem">Movies</li>
+          <li className="menuItem">TV Shows</li>
+          <li className="menuItem">
+            <HiOutlineSearch />
+          </li>
+        </ul>
+
+        <div className="mobileMenuItems">
+          <HiOutlineSearch onClick={() => setMobileMenu(false)} />
+          {mobileMenu ? (
+            <VscChromeClose onClick={openMobileMenu} />
+          ) : (
+            <SlMenu onClick={openMobileMenu} />
+          )}
+        </div>
+      </ContentWrapper>
+      <div className="searchBar">
+        <ContentWrapper>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or TV show..."
+              onChange={(e) => setQuery(e.target.value)}
+              // onKeyUp={searchQueryHandler}
+            />
+            <VscChromeClose onClick={openMobileMenu} />
+          </div>
+        </ContentWrapper>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
