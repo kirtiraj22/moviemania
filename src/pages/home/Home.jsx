c540bc0a8bd349de008b6c9bf812b4375a62d482
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-07-22 22:36:16 +0530

    initialized carousel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
+      <Trending />
     </div>
   );
 };
