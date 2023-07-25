c5baa431c38a6592f0662f98251cb54bdc743e4d
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-07-25 23:08:03 +0530

    added main carousel && skeleton loading

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import useFetch from "../../../hooks/useFetch";
+import Carousel from "../../../components/carousel/Carousel";
 
 const Trending = () => {
   const [endPoint, setEndPoint] = useState("day");
@@ -17,6 +18,7 @@ const Trending = () => {
         <span className="carouselTitle">Trending</span>
         <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} />
     </div>
   );
 };
